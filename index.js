4788605a51eee4fd0d7557ac2dca968cddbfa241
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-05-11 22:43:41 +0200

    add function to get all Users from one league

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import credentials from "./credentials.js";
 import login from "./lib/api/login.js";
 import myLeagueInfo from "./lib/api/my_league_info.js";
 import getLeagues from "./lib/api/leagues.js";
+import leagueUsers from "./lib/api/league_users.js";
 
 async function main() {
     let { user, token, leagues } = await login(
@@ -14,9 +15,10 @@ async function main() {
     console.log(leagues);
     const league = await myLeagueInfo(token, leagues[0].id);
     console.log(league);
-    console.log("Test-----------------");
     leagues = await getLeagues(token);
     console.log(leagues);
+    let users = await leagueUsers(token, leagues[0].id);
+    console.log(users);
 }
 
 main();
