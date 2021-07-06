3f8c531e558358ed67542557da4476473a3786d7
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-07-06 18:19:36 +0200

    adding first version of league_stats, user_feed and user_stats

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import myLeagueInfo from "./lib/api/my_league_info.js";
 import getLeagues from "./lib/api/leagues.js";
 import leagueUsers from "./lib/api/league_users.js";
 import userProfile from "./lib/api/user_profile.js";
+import userFeed from "./lib/api/user_feed.js";
+import leagueStats from "./lib/api/league_stats.js";
+import userStats from "./lib/api/user_stats.js";
 import { Manager } from "./lib/models/manager.js";
 
 async function main() {
@@ -12,9 +15,6 @@ async function main() {
         credentials.password
     );
     if (user === undefined) throw new Error("could not login.");
-    console.log(token);
-    console.log(user);
-    console.log(leagues);
     const league = await myLeagueInfo(token, leagues[0].id);
     console.log(league);
     leagues = await getLeagues(token);
@@ -26,7 +26,10 @@ async function main() {
     console.log(profile.seasons[0].matchDays);
     let manager = new Manager(profile, leagues[0]);
     console.log(manager);
-    console.log(manager.seasons[0].matchDays);
+    console.log("##################################");
+    await userFeed(token, leagues[0].id, user.id);
+    await leagueStats(token, leagues[0].id);
+    await userStats(token, leagues[0].id, user.id); //1707891
 }
 
 main();
